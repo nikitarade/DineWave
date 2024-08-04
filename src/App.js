10a5c2b17188230fdaa9fee95ab3bea94e4ab609
Nikita Rade <nikitarade2003@gmail.com>
2024-08-04 23:09:54 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
-import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
-import MenuPage from './components/MenuPage';
-import ContactPage from './components/ContactPage';
 import './App.css';
 
 function App() {
@@ -13,10 +10,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/home" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
-          <Route path="/menu" element={<MenuPage />} />
-          <Route path="/contact" element={<ContactPage />} />
         </Routes>
       </div>
     </Router>
--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -16,19 +16,21 @@ const AboutPage = () => {
       </nav>
       <div className="about-content">
         <h1>About DineWave</h1>
-        <img src="/images/logo_dinewave.png" alt="DineWave Logo" className="about-logo" />
-        <p className="description">DineWave is dedicated to providing the best dining experience.</p>
-        <div className="mission-vision">
-          <div className="mission">
-            <h2>Our Mission:</h2>
-            <p>We aim to revolutionize the dining industry by integrating advanced technology with exceptional culinary experiences.</p>
-          </div>
-          <div className="vision">
-            <h2>Our Vision:</h2>
-            <p>To be the leading provider of innovative restaurant management solutions worldwide.</p>
+        <img src="/images/logo_dinewave.png" alt="Sound Wave" className="sound-wave" />
+        <p className="about-description">DineWave is dedicated to providing the best dining experience.</p>
+        <div className="content-wrapper">
+          <img src="/images/about.jpg" alt="Illustration" className="about-image" />
+          <div className="mission-vision">
+            <div className="mission">
+              <h2>Our Mission:</h2>
+              <p>We aim to revolutionize the dining industry by integrating advanced technology with exceptional culinary experiences.</p>
+            </div>
+            <div className="vision">
+              <h2>Our Vision:</h2>
+              <p>To be the leading provider of innovative restaurant management solutions worldwide.</p>
+            </div>
           </div>
         </div>
-        <img src="/images/about.jpg" alt="Illustration" className="about-image" />
       </div>
     </div>
   );
