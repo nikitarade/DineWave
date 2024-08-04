17ffe1dc446bc7d52a110b33f053084b893f2eb7
Nikita Rade <nikitarade2003@gmail.com>
2024-08-04 23:23:02 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
+import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
+import MenuPage from './components/MenuPage';
+import ContactPage from './components/ContactPage';
 import './App.css';
 
 function App() {
@@ -10,7 +13,10 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/menu" element={<MenuPage />} />
+          <Route path="/contact" element={<ContactPage />} />
         </Routes>
       </div>
     </Router>
